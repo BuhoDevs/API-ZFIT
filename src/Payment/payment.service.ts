afb014dd9d1e72bb78d2ca8e375365db7a673a6a
josemaria <devfarm.cheyo@gmail.com>
2024-07-13 15:54:12 -0400

    agregar monthly revenue

--- src/Payment/payment.service.ts
+++ src/Payment/payment.service.ts
@@ -8,3 +8,26 @@ export const findPaymentBysubscriptionId = async (
     where: { subscriptionId },
   });
 };
+
+export const findMonthlyRevenue = async ({
+  startDate,
+  endDate,
+}: {
+  startDate: Date;
+  endDate: Date;
+}) => {
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      transactionAmmount: true,
+    },
+    where: {
+      createdAt: {
+        gte: startDate,
+        lte: endDate,
+      },
+      status: true,
+    },
+  });
+
+  return totalRevenue._sum.transactionAmmount || 0;
+};
